6cb5e5cfb3bfb452d2ee7d3dfa3980957320eb08
Quinn <qle17@uclive.ac.nz>
2024-04-26 16:12:38 +1200

    Added functionality to display towers and upgrades in the inventory menu.

--- src/main/java/seng201/team0/gui/InventoryController.java
+++ src/main/java/seng201/team0/gui/InventoryController.java
@@ -1,14 +1,19 @@
 package seng201.team0.gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import seng201.team0.GameManager;
+import seng201.team0.models.Tower;
+import seng201.team0.models.Upgrade;
 import seng201.team0.services.CurrentRoundService;
 import seng201.team0.services.MoneyBalanceService;
 import seng201.team0.services.RoundsSelectionService;
+import seng201.team0.services.InventoryService;
 
 /**
  * Controller for the inventory.fxml window
@@ -20,6 +25,10 @@ public class InventoryController {
     private RoundsSelectionService roundsService;
     private MoneyBalanceService moneyService;
     private CurrentRoundService currentRoundService;
+    private InventoryService inventoryService;
+    private Tower selectedMainTower;
+    private Tower selectedReserveTower;
+    private Upgrade selectedUpgrade;
 
     @FXML
     public BorderPane inventoryBorderPane;
@@ -28,6 +37,10 @@ public class InventoryController {
     public Label currentMoneyLabel;
     public Label currentRoundLabel;
     public Label roundsRemainingLabel;
+    public ListView<Tower> mainTowerList;
+    public ListView<Tower> reserveTowerList;
+    public ListView<Upgrade> upgradeList;
+
 
     /**
      * Constructor
@@ -39,6 +52,7 @@ public class InventoryController {
         this.roundsService = gameManager.getRoundsService();
         this.moneyService = gameManager.getMoneyService();
         this.currentRoundService = gameManager.getCurrentRoundService();
+        this.inventoryService = gameManager.getInventoryService();
     }
     /**
      * Initialize the window
@@ -53,6 +67,26 @@ public class InventoryController {
         currentRoundLabel.setText(currentRoundService.getCurrentRound().toString());
         roundsRemainingLabel.setText(Integer.toString(remainingRounds));
         System.out.println("Inventory Page");
+
+
+        mainTowerList.setCellFactory(new TowerCellFactory());
+        mainTowerList.setItems(FXCollections.observableArrayList(inventoryService.getMainTowerSelection()));
+        // Used Tut 3 code here as a template
+        mainTowerList.getSelectionModel().getSelectedItems().addListener((ListChangeListener<Tower>) r -> {
+            selectedMainTower = mainTowerList.getSelectionModel().getSelectedItem();
+        });
+
+        reserveTowerList.setCellFactory(new TowerCellFactory());
+        reserveTowerList.setItems(FXCollections.observableArrayList(inventoryService.getReserveTowerSelection()));
+        reserveTowerList.getSelectionModel().getSelectedItems().addListener((ListChangeListener<Tower>) r -> {
+            selectedReserveTower = reserveTowerList.getSelectionModel().getSelectedItem();
+        });
+
+        upgradeList.setCellFactory(new UpgradeCellFactory());
+        upgradeList.setItems(FXCollections.observableArrayList(inventoryService.getUserUpgrades()));
+        upgradeList.getSelectionModel().getSelectedItems().addListener((ListChangeListener<Upgrade>) r -> {
+            selectedUpgrade = upgradeList.getSelectionModel().getSelectedItem();
+        });
     }
     /**
      * Method to call when the back button is clicked
