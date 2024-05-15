860fd633f5982ac5fd5d5a3580f085e387bdc7f2
Quinn <qle17@uclive.ac.nz>
2024-05-15 14:04:39 +1200

    Added test class for Cart class, has tests for filling the cart and when filling the cart results in it overflowing.

---
+++ src/test/java/seng201/team0/unittests/models/CartTest.java
@@ -0,0 +1,31 @@
+package seng201.team0.unittests.models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seng201.team0.models.Cart;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+
+    private Cart testCart;
+
+    @Test
+    void fill() {
+        testCart = new Cart(5, null, 0);
+        assertEquals(0, testCart.getFilledSize());
+        testCart.fill(2);
+        assertEquals(2, testCart.getFilledSize());
+        testCart.fill(1);
+        assertEquals(3, testCart.getFilledSize());
+    }
+    @Test
+    void fill_overflow() {
+        testCart = new Cart(3, null, 0);
+        assertEquals(0, testCart.getFilledSize());
+        testCart.fill(4);
+        assertEquals(3, testCart.getFilledSize());
+    }
+}
