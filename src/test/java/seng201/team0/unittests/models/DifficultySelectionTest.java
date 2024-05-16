25f782f22e6989e13b9d738415e4cdea58b63663
Caleb <cco139@uclive.ac.nz>
2024-05-16 22:37:54 +1200

    Added 2 new tests

--- src/test/java/seng201/team0/unittests/models/DifficultySelectionTest.java
+++ src/test/java/seng201/team0/unittests/models/DifficultySelectionTest.java
@@ -1,4 +1,23 @@
 package seng201.team0.unittests.models;
 
-public class DifficultySelectionTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seng201.team0.models.DifficultySelection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DifficultySelectionTest {
+    private DifficultySelection difficultySelectionTest;
+
+    @BeforeEach
+    void setupTest() {
+        difficultySelectionTest = new DifficultySelection();
+    }
+
+    @Test
+    void testDifficultySelection() {
+        assertEquals("Easy", difficultySelectionTest.getDifficulty());
+        difficultySelectionTest.setDifficulty("Medium");
+        assertEquals("Medium", difficultySelectionTest.getDifficulty());
+    }
 }
