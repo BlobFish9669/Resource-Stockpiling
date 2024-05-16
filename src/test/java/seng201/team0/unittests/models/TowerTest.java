b8cb7fc38760bbea9c9ce0ebafc348d8de9b60e8
Quinn <qle17@uclive.ac.nz>
2024-05-17 11:10:46 +1200

    Added tests for applying upgrade for a couple of different upgrade types. Still need to do tests for other upgrades

--- src/test/java/seng201/team0/unittests/models/TowerTest.java
+++ src/test/java/seng201/team0/unittests/models/TowerTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import seng201.team0.models.Tower;
+import seng201.team0.models.Upgrade;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TowerTest {
     private Tower testTower;
+    private Upgrade testUpgrade;
     @BeforeEach
     void setupTest() {
         testTower = new Tower("Stone", 1, 1, 1, 30);
@@ -35,5 +37,20 @@ public class TowerTest {
         assertEquals(16, testTower.getResourceAmount());
         assertEquals(0.25, testTower.getReloadSpeed());
         assertEquals(64, testTower.getFillRate());
+    }
+    @Test
+    void testUpgradeChangeResource() {
+        testUpgrade = new Upgrade("TestUpgrade", "Resource Type", "Gold", 20, 1.0);
+        assertEquals("Stone", testTower.getResourceType());
+        testTower.applyUpgrade(testUpgrade);
+        assertEquals("Gold", testTower.getResourceType());
+    }
+    @Test
+    void testUpgradeAddPoints() {
+        testUpgrade = new Upgrade("TestUpgrade", "Tower Points", 20, 150.0);
+        assertEquals(0, testTower.getTowerPoints());
+        testTower.applyUpgrade(testUpgrade);
+        assertEquals(50, testTower.getTowerPoints());
+        assertEquals(2, testTower.getLevel());
     }
 }
