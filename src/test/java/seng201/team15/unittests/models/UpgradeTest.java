fdbac469e56fb57f8e6d514ebbc68a0bd5afcac4
Quinn <qle17@uclive.ac.nz>
2024-05-17 20:48:01 +1200

    Implemented test for Upgrade class, particularly looking a the overloaded constructor

--- src/test/java/seng201/team15/unittests/models/UpgradeTest.java
+++ src/test/java/seng201/team15/unittests/models/UpgradeTest.java
@@ -1,4 +1,24 @@
 package seng201.team15.unittests.models;
 
+import seng201.team15.models.Upgrade;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UpgradeTest {
+    private Upgrade testUpgrade;
+    @Test
+    void testUpgrades() {
+        testUpgrade = new Upgrade("TestUpgrade", "Resource Type", "Stone", 20, 1.0);
+        assertEquals("Resource Type", testUpgrade.getUpgradeType());
+        assertEquals("Stone", testUpgrade.getResourceType());
+
+        testUpgrade = new Upgrade("TestUpgrade", "Tower Points", 20, 1.0);
+        assertEquals("TestUpgrade", testUpgrade.getUpgradeTitle());
+        assertNull(testUpgrade.getResourceType());
+        assertEquals(20, testUpgrade.getCost());
+        assertEquals(1.0, testUpgrade.getUpgradeModifierAmount());
+        assertEquals(15, testUpgrade.getSellPrice());
+    }
 }
