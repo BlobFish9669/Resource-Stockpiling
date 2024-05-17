d94169f0cb895dcb3eef6edc8de66712c30fd26c
Quinn <qle17@uclive.ac.nz>
2024-05-17 20:50:44 +1200

    Minor change to upgrade test

--- src/test/java/seng201/team15/unittests/models/UpgradeTest.java
+++ src/test/java/seng201/team15/unittests/models/UpgradeTest.java
@@ -1,7 +1,6 @@
 package seng201.team15.unittests.models;
 
 import seng201.team15.models.Upgrade;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,11 +8,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UpgradeTest {
     private Upgrade testUpgrade;
     @Test
-    void testUpgrades() {
+    void testUpgradeResource() {
         testUpgrade = new Upgrade("TestUpgrade", "Resource Type", "Stone", 20, 1.0);
         assertEquals("Resource Type", testUpgrade.getUpgradeType());
         assertEquals("Stone", testUpgrade.getResourceType());
-
+    }
+    @Test
+    void testUpgrade() {
         testUpgrade = new Upgrade("TestUpgrade", "Tower Points", 20, 1.0);
         assertEquals("TestUpgrade", testUpgrade.getUpgradeTitle());
         assertNull(testUpgrade.getResourceType());
