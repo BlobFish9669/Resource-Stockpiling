5e7e82429ff2f5457d3c8b80b0a9ed37825ec5c0
qle17 <qle17@uclive.ac.nz>
2024-05-22 11:53:25 +1200

    Finished javadocs for the ShopAvailabilityService test.

--- src/test/java/seng201/team15/unittests/services/ShopAvailabilityServiceTest.java
+++ src/test/java/seng201/team15/unittests/services/ShopAvailabilityServiceTest.java
@@ -9,13 +9,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class ShopAvailabilityServiceTest {
     ShopAvailabilityService shopAvailabilityServiceTest;
 
+    /**
+     * Initialise a new ShopAvailabilityService class before each test.
+     */
     @BeforeEach
     void setUp() {
         shopAvailabilityServiceTest = new ShopAvailabilityService();
     }
 
+    /**
+     * Test for buying towers.
+     * After initialisation, the test checks that the following 4 attributes have been constructed correctly:
+     * The test checks that the number of towers available to purchase is between 3 and 5 inclusive,
+     * It checks that the list of available towers has the same size as the number of available towers,
+     * It also checks that the purchased tower list (a list of boolean values indicated whether a specified tower has been purchased) has
+     * the same size as the number of towers available,
+     * It then checks that there are no towers that have been purchased.
+     * The test then sets the first 2 towers as purchased, and checks that the total number of purchased towers is only 2.
+     * It lastly checks that the first 2 towers have been acknowledged as being purchased in the purchased towers list.
+     */
     @Test
-    void testingTowersBuyAndSell() {
+    void testingTowersBuy() {
         shopAvailabilityServiceTest.setTotalRounds(5);
         shopAvailabilityServiceTest.resetStore(1);
 
@@ -37,6 +51,16 @@ class ShopAvailabilityServiceTest {
         assertTrue(shopAvailabilityServiceTest.getPurchasedTowers().get(0) && shopAvailabilityServiceTest.getPurchasedTowers().get(1));
     }
 
+    /**
+     * Test for resetting the shop.
+     * The shop is initially reset, then the test checks that the following 4 attributes have been reset correctly:
+     * The test checks that the number of towers available to purchase is between 3 and 5 inclusive,
+     * It checks that the list of available towers has the same size as the number of available towers,
+     * It also checks that the purchased tower list (a list of boolean values indicated whether a specified tower has been purchased) has
+     * the same size as the number of towers available,
+     * It then checks that there are no towers that have been purchased.
+     * The test then indicates the first 2 towers as purchased and ensures that they are updated correctly.
+     */
     @Test
     void testingTowerShopReset() {
         shopAvailabilityServiceTest.setTotalRounds(5);
@@ -60,8 +84,20 @@ class ShopAvailabilityServiceTest {
         assertTrue(shopAvailabilityServiceTest.getPurchasedTowers().get(0) && shopAvailabilityServiceTest.getPurchasedTowers().get(1));
     }
 
+    /**
+     * Test for buying upgrades.
+     * The shop is initially reset, then the test checks that the following 4 attributes have been reset correctly:
+     * The test checks that the number of upgrades available to purchase is between 3 and 6 inclusive,
+     * It checks that the list of available upgrades has the same size as the number of available upgrades,
+     * It also checks that the purchased upgrade list (a list of boolean values indicated whether a specified upgrade has been purchased) has
+     * the same size as the number of upgrades available,
+     * It then checks that there are no upgrades that have been purchased.
+     * The shop then sets the first 2 upgrades as purchased.
+     * The test iterates through the purchased upgrade list, and checks that only 2 upgrades are marked as purchased.
+     * It also checks that the first and second upgrades have been indicated as purchased.
+     */
     @Test
-    void testingUpgradesBuyAndSell() {
+    void testingUpgradesBuy() {
         shopAvailabilityServiceTest.setTotalRounds(5);
         shopAvailabilityServiceTest.resetStore(1);
 
@@ -83,6 +119,18 @@ class ShopAvailabilityServiceTest {
         assertTrue(shopAvailabilityServiceTest.getPurchasedUpgrades().get(0) && shopAvailabilityServiceTest.getPurchasedUpgrades().get(1));
     }
 
+    /**
+     * Test for resetting the upgrades portion of the shop.
+     * The shop is initially reset, then the test checks that the following 4 attributes have been reset correctly:
+     * The test checks that the number of upgrades available to purchase is between 3 and 6 inclusive,
+     * It checks that the list of available upgrades has the same size as the number of available upgrades,
+     * It also checks that the purchased upgrade list (a list of boolean values indicated whether a specified upgrade has been purchased) has
+     * the same size as the number of upgrades available,
+     * It then checks that there are no upgrades that have been purchased.
+     * The shop then sets the first 2 upgrades as purchased.
+     * The test iterates through the purchased upgrade list, and checks that only 2 upgrades are marked as purchased.
+     * It also checks that the first and second upgrades have been indicated as purchased.
+     */
     @Test
     void testingUpgradesShopReset() {
         shopAvailabilityServiceTest.setTotalRounds(5);
